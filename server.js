5c653a7f43a4907d175479ddac580c6627167815
webfunny <webfunny@sina.com>
2018-06-05 21:51:50 +0800

    test

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
-var path = require('path');
 var fs = require("fs");
+const promisify = require('util').promisify;
+const writeFile = fs.writeFile;
+const mkdirp = require('mkdirp');
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/add',function(req,res,next){
 		const type = req.body['type'];
+		const java_dir = '../java/'+type;
+		// Create model direactor
+		mkdirp(java_dir);
 		const fileName = req.body['fileName'];		
 		const beanText = req.body['beanText'];
-    fs.writeFile('java/'+type+'/'+fileName,beanText);
+    writeFile(java_dir+'/'+fileName,beanText);
 		res.send('Ok');
 });
 
