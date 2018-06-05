a031975b204448eb39dc2ec8d3e8c064f0d8675f
zhangxuesheng <zhangxuesheng@forte.com.cn>
2018-06-05 18:38:38 +0800

    java bean

---
+++ java/seller/OrderPriceHistory.java
@@ -0,0 +1,95 @@
+/**
+  * Copyright 2018 Risingsun 
+  */
+package com.lvpeng.shop.bean;
+import java.util.Date;
+
+/**
+ * Auto-generated: 2018-06-05 18:37:39
+ *
+ * @author Risingsun (Risingsun)
+ * @website 
+ */
+public class OrderPriceHistory {
+
+    private int orderId;
+    private double currentPrice;
+    private double modifyPrice;
+    private int currentPost;
+    private int modifyPost;
+    private String currentUUID;
+    private String modifyUUID;
+    private double finalPrice;
+    private Date modifyTime;
+    private String remark;
+    public void setOrderId(int orderId) {
+         this.orderId = orderId;
+     }
+     public int getOrderId() {
+         return orderId;
+     }
+
+    public void setCurrentPrice(double currentPrice) {
+         this.currentPrice = currentPrice;
+     }
+     public double getCurrentPrice() {
+         return currentPrice;
+     }
+
+    public void setModifyPrice(double modifyPrice) {
+         this.modifyPrice = modifyPrice;
+     }
+     public double getModifyPrice() {
+         return modifyPrice;
+     }
+
+    public void setCurrentPost(int currentPost) {
+         this.currentPost = currentPost;
+     }
+     public int getCurrentPost() {
+         return currentPost;
+     }
+
+    public void setModifyPost(int modifyPost) {
+         this.modifyPost = modifyPost;
+     }
+     public int getModifyPost() {
+         return modifyPost;
+     }
+
+    public void setCurrentUUID(String currentUUID) {
+         this.currentUUID = currentUUID;
+     }
+     public String getCurrentUUID() {
+         return currentUUID;
+     }
+
+    public void setModifyUUID(String modifyUUID) {
+         this.modifyUUID = modifyUUID;
+     }
+     public String getModifyUUID() {
+         return modifyUUID;
+     }
+
+    public void setFinalPrice(double finalPrice) {
+         this.finalPrice = finalPrice;
+     }
+     public double getFinalPrice() {
+         return finalPrice;
+     }
+
+    public void setModifyTime(Date modifyTime) {
+         this.modifyTime = modifyTime;
+     }
+     public Date getModifyTime() {
+         return modifyTime;
+     }
+
+    public void setRemark(String remark) {
+         this.remark = remark;
+     }
+     public String getRemark() {
+         return remark;
+     }
+
+}
---
+++ java/seller/TestRequest.java
@@ -0,0 +1,22 @@
+/**
+  * Copyright 2018 Risingsun 
+  */
+package com.lvpeng.shop.bean;
+
+/**
+ * Auto-generated: 2018-06-05 18:38:10
+ *
+ * @author Risingsun (Risingsun)
+ * @website 
+ */
+public class TestRequest {
+
+    private String result;
+    public void setResult(String result) {
+         this.result = result;
+     }
+     public String getResult() {
+         return result;
+     }
+
+}
